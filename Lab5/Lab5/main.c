ee9f527ab7a8f6638450c140435feb65d5bf7588
Cole Brooks <cole.brooks@hotmail.com>
2021-04-08 10:29:52 -0500

    initial commit

---
+++ Lab5/Lab5/main.c
@@ -0,0 +1,19 @@
+/*
+ * Lab5.c
+ *
+ * Created: 4/8/2021 10:24:57 AM
+ * Author : Cole Brooks
+ *			Thomas Butler
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
