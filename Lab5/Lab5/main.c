e01a93542064b297a3fca60077e2c37b6b0f7c5c
Cole Brooks <cole.brooks@hotmail.com>
2021-04-16 15:55:43 -0500

    Implemented Serial Communication functionality

--- Lab5/Lab5/main.c
+++ Lab5/Lab5/main.c
@@ -8,43 +8,70 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
+#define F_CPU 16000000L // 16 MHz
+#define USART_BAUDRATE 9600
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) -1)
 ////////////////////////////////////////////////////////////////////
 //
-// Function: TWIInit
+// Function: usart_init
 //
-// Purpose: Initializes registers necessary for ATMega328p 
-//			to communicate via 2-wire interface
+// Arguments: Void 
+// 
+// Purpose: set baud rate, enable rx and tx, and set
+//			frame formate: 8 data, 2 stop bit 
 //			
 ////////////////////////////////////////////////////////////////////
-void TWIInit(void)
+void usart_init(void)
 {
-	
-	
+	// Set baud rate
+	UBRR0H = (uint8_t)(UBRR_VALUE>>8);
+	UBRR0L = (uint8_t)UBRR_VALUE;
+	// Set frame format to 8 data bits, no parity, 1 stop bit
+	UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);
+	//enable transmission and reception
+	UCSR0B |= (1<<RXEN0)|(1<<TXEN0);
 }
 
 ////////////////////////////////////////////////////////////////////
-// Function: TWIStart
 //
-// Purpose: Send the start signal for the 2-wire interface
+// Function: usart_tx
+//
+// Arguments: uint8_t data:
+//
+// Purpose: Sends data to serial monitor
 //
 ////////////////////////////////////////////////////////////////////
-void TWIStart(void)
+void usart_tx(uint8_t data)
 {
-	
-	
+	//wait while previous byte is completed
+	while(!(UCSR0A&(1<<UDRE0))){};
+	// Transmit data
+	UDR0 = data;
 }
 
 ////////////////////////////////////////////////////////////////////
-// Function: TWIStop
 //
-// Purpose: Send the stop signal for the 2-wire interface
+// Function: usart_rx
+//
+// Arguments: Void
+//
+// Purpose: Receives data from serial monitor
 //
 ////////////////////////////////////////////////////////////////////
-void TWIStop(void)
+void usart_rx(void)
 {
-	
-	
+	// Wait for byte to be received
+	while(!(UCSR0A&(1<<RXC0))){};
+	// Return received data
+	if(UDR0 == 71)
+	{
+		usart_tx(118); // v
+		usart_tx(32); // space
+		usart_tx(61); // =
+		usart_tx(10); // \n
+	}
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -54,13 +81,14 @@ void TWIStop(void)
 //
 ////////////////////////////////////////////////////////////////////
 int main(void)
-{
+{	
 	// Initializations
-    TWIInit();
-	
+	usart_init();    
+
 	// main loop
     while (1) 
     {
+		usart_rx();
     }
 }
 
